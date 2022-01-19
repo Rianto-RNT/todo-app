b17c6f2630d76801c439007c2926eebd4162453f
Rianto <rianto.rnt@gmail.com>
2022-01-19 22:06:26 +0800

    first commit

---
+++ 02-backend/restful-web-services/src/test/java/com/rian/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rian.rest.webservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
