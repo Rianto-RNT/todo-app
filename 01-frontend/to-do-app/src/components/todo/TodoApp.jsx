a2e3f33b6dc8cc34c09db5d84dfb26144b99a0b3
Rianto <rianto.rnt@gmail.com>
2022-01-20 08:32:09 +0800

    Adding an error component for invalid URIs

--- 01-frontend/to-do-app/src/components/todo/TodoApp.jsx
+++ 01-frontend/to-do-app/src/components/todo/TodoApp.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class TodoApp extends Component {
   render() {
@@ -7,9 +7,12 @@ class TodoApp extends Component {
       <div className="TodoApp">
         <Router>
           <>
-            <Route path="/" exact component={LoginComponent} />
-            <Route path="/login" component={LoginComponent} />
-            <Route path="/welcome" component={WelcomeComponent} />
+            <Switch>
+              <Route path="/" exact component={LoginComponent} />
+              <Route path="/login" component={LoginComponent} />
+              <Route path="/welcome" component={WelcomeComponent} />
+              <Route component={ErrorComponent} />
+            </Switch>
           </>
         </Router>
         {/* <LoginComponent></LoginComponent>
@@ -23,6 +26,10 @@ class WelcomeComponent extends Component {
   render() {
     return <div>Welcome to RNT to App</div>;
   }
+}
+
+function ErrorComponent() {
+  return <div>An Error Occurred. I don't know what to do! please contact support at rianto.rnt@gmail.com</div>;
 }
 
 // login component class
@@ -65,7 +72,7 @@ class LoginComponent extends Component {
   loginClicked() {
     // RNT-todo, dummy
     if (this.state.username === 'RNT-todo' && this.state.password === 'dummy') {
-      this.props.history.push("/welcome")
+      this.props.history.push('/welcome');
       // this.setState({ showSuccessMessage: true });
       // this.setState({ hasLoginFailed: false });
     } else {
@@ -84,7 +91,8 @@ class LoginComponent extends Component {
         {this.state.hasLoginFailed && <div>Invalid Credentials</div>}
         {this.state.showSuccessMessage && <div>Login Successful</div>}
         User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange}></input>
-        Password:<input type="password" name="password" value={this.state.password} onChange={this.handleChange}></input>
+        Password:
+        <input type="password" name="password" value={this.state.password} onChange={this.handleChange}></input>
         <button onClick={this.loginClicked}>Login</button>
       </div>
     );
