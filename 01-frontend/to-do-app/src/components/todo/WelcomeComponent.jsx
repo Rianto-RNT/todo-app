9f6555f2e30e7e519c900a16e213a1404e775f15
Rianto <rianto.rnt@gmail.com>
2022-01-27 12:32:00 +0800

    connecting react with restful API #5 - allow cross origin request

--- 01-frontend/to-do-app/src/components/todo/WelcomeComponent.jsx
+++ 01-frontend/to-do-app/src/components/todo/WelcomeComponent.jsx
@@ -6,6 +6,10 @@ class WelcomeComponent extends Component {
   constructor(props) {
     super(props);
     this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this);
+    this.state = {
+      welcomeMessage: ''      
+    }
+    this.handleSuccessfulResponse = this.handleSuccessfulResponse.bind(this);
   }
 
   render() {
@@ -21,14 +25,22 @@ class WelcomeComponent extends Component {
             Get Welcome Message
           </button>
         </div>
+        <div className='container'>
+          { this.state.welcomeMessage }
+        </div>
       </>
     );
   }
 
   retrieveWelcomeMessage() {
     HelloWorldService.executeHelloWorldService()
-    .then(response => console.log(response))
+    .then(response => this.handleSuccessfulResponse(response))
     // .catch();
+  }
+
+  handleSuccessfulResponse(response) {
+    this.setState({welcomeMessage: response.data})
+    
   }
 }
 
